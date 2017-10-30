54f330eaf7799c45a0b59014e2a7b5f97651d710
Andrey Chechel <andrey@object.net>
2017-10-30 16:49:57 +0300

    Update Bridge packages

--- ChromeDemo/ChromeDemo/Unpacked/js/ChromeDemo.js
+++ ChromeDemo/ChromeDemo/Unpacked/js/ChromeDemo.js
@@ -1,7 +1,7 @@
 /**
  * @version 1.0.0.0
  * @copyright Copyright ©  2017
- * @compiler Bridge.NET 16.4.0
+ * @compiler Bridge.NET 16.4.1
  */
 Bridge.assembly("ChromeDemo", function ($asm, globals) {
     "use strict";
