ee39aa710fac08ba0dc8231b8d7532d203dbcb68
Andrey Chechel <andrey@object.net>
2018-01-16 17:36:15 +0300

    Update Dependencies

--- dist/pixi.js/js/PixiJsDemo.js
+++ dist/pixi.js/js/PixiJsDemo.js
@@ -1,7 +1,7 @@
 /**
  * @version 1.0.0.0
  * @copyright Copyright ©  2017
- * @compiler Bridge.NET 16.6.0
+ * @compiler Bridge.NET 16.6.1
  */
 Bridge.assembly("PixiJsDemo", function ($asm, globals) {
     "use strict";
